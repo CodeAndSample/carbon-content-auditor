6f45588b87ed0e776934f6b0df32605e03cede8d
qjk <quifunk@gmail.com>
2021-06-21 10:13:14 +0200

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import HeaderBaseWNavigationActionsAndSideNav from "../components/HeaderBaseWNavigationActionsAndSideNav";
 
 export default function Home() {
